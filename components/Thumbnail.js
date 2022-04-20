08dafb089c9c347517e2939c8359711db658e8f7
tosin adefope <sirtosin45@gmail.com>
2022-04-20 17:12:49 +0100

    final

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -5,7 +5,7 @@ import { forwardRef } from "react";
 const Thumbnail = forwardRef(({ result }, ref) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
   return (
-    <div ref={ref} className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+    <div ref={ref} className="p-6 ml-[8em] sm:ml-0 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50 ">
       <Image
         layout="responsive"
         height={1080}
